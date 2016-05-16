07f5143885ea13230ddc07dd0891635d193de99f
Deivid <deividscriollo@gmail.com>
2016-05-16 09:14:50 -0500

    procesos

--- app/Curiosidades.php
+++ app/Curiosidades.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Curiosidades extends Model
 {
-        protected $table='curiosidades';
+    protected $table='curiosidades';
     protected $fillable = ['id_curiosidades', 'titulo', 'descripcion','URL','refencia','img','estado'];
 }
--- app/Farandula.php
+++ app/Farandula.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Farandula extends Model
 {
-      protected $table='farandula';
+    protected $table='farandula';
     protected $fillable = ['id_farandula', 'titulo', 'descripcion','URL','refencia','img','estado'];
 }
